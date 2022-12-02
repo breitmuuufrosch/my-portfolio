6735b8aaa4a89dd7cf508277cbc9a090a295c20e
Andreas <breitmuuufrosch@gmail.com>
2022-12-03 00:51:06 +0100

    #9: Refactor API and DB handling with promises

--- backend/src/routes/currencyRouter.ts
+++ backend/src/routes/currencyRouter.ts
@@ -5,45 +5,30 @@ import { Currency } from '../types/currency';
 const currencyRouter = express.Router();
 
 currencyRouter.get('/', async (req: Request, res: Response) => {
-  currencyModel.findAll((err: Error, currencies: Currency[]) => {
-    if (err) {
-      return res.status(500).json({ errorMessage: err.message });
-    }
-
-    res.status(200).json({ data: currencies });
-  });
+  currencyModel.findAll()
+    .then((currencies: Currency[]) => { res.status(200).json({ data: currencies }); })
+    .catch((err: Error) => { res.status(500).json({ errorMessage: err.message }); });
 });
 
 currencyRouter.post('/', async (req: Request, res: Response) => {
   const newCurrency: Currency = req.body;
-  currencyModel.create(newCurrency, (err: Error, symbol: number) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-
-    res.status(200).json({ symbol });
-  });
+  currencyModel.create(newCurrency)
+    .then((symbol: number) => { res.status(200).json({ symbol }); })
+    .catch((err: Error) => { res.status(500).json({ message: err.message }); });
 });
 
 currencyRouter.get('/:symbol', async (req: Request, res: Response) => {
   const symbol = req.params.symbol;
-  currencyModel.findOne(symbol, (err: Error, currency: Currency) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    res.status(200).json({ data: currency });
-  });
+  currencyModel.findOne(symbol)
+    .then((currency: Currency) => { res.status(200).json({ data: currency }); })
+    .catch((err: Error) => { res.status(500).json({ message: err.message }); });
 });
 
 currencyRouter.put('/:symbol', async (req: Request, res: Response) => {
   const currency: Currency = req.body;
-  currencyModel.update(currency, (err: Error) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-
-    res.status(200).send();
-  });
+  currencyModel.update(currency)
+    .then(() => { res.status(200).send(); })
+    .catch((err: Error) => { res.status(500).json({ message: err.message }); });
 });
 
 export { currencyRouter };
---
+++ backend/src/routes/security/transactionRouter.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import * as securityModel from "../../models/security";
+import { Security, SecurityTransaction } from '../../types/security';
+
+const transactionRouter = express.Router();
+
+transactionRouter.post('/', async (req: Request, res: Response) => {
+  const transaction = req.body as SecurityTransaction;
+
+  securityModel.findOne(transaction.symbol)
+    .then((security) => securityModel.createTransaction({ ...transaction, security_id: security.id }))
+    .then((insertedIds) => { res.status(200).json({ data: insertedIds }); })
+    .catch((err: any) => { res.status(500).json({ message: err.message }); });
+});
+
+transactionRouter.post('/multiple', async (req: Request, res: Response) => {
+  const transactions = req.body as SecurityTransaction[];
+
+  Promise.all(transactions.map(item => {
+    return new Promise((resolve, reject) => {
+      securityModel.findOne(item.symbol)
+        .then((security: Security) => {
+          if (item.currency === 'CHF') {
+            securityModel.createTransaction({ ...item, security_id: security.id })
+              .then(resolve)
+              .catch(reject);
+          } else {
+            securityModel.createTransactionForeign({ ...item, security_id: security.id })
+              .then(resolve)
+              .catch(reject);
+          }
+        })
+        .catch((err: any) => { reject(err); });
+    });
+  }))
+    .then((result) => { res.status(200).json({ data: result }); })
+    .catch((err: any) => { res.status(500).json({ message: err.message }); });
+});
+
+export { transactionRouter };
